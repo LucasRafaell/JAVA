6018a926ec365d8f9f8e3f436c01840d2f1d54df
Lucas Rafael <lucasrafaelinfo@gmail.com>
2023-04-05 22:12:14 -0300

    Leitura e ordenação de arquivo com a interface comparable

--- src/entities/Employee.java
+++ src/entities/Employee.java
@@ -1,14 +1,20 @@
 package entities;
 
-public class Employee {
+public class Employee implements Comparable<Employee> {
 		
 	private String name;
 	private Integer hours;
 	private Double valuePerHour;
+	private Double salary;
 	
 	public Employee() {		
 	}
 
+	public Employee(String name, Double salary) {
+		this.name = name;
+		this.salary = salary;
+	}
+	
 	public Employee(String name, Integer hours, Double valuePerHour) {
 		this.name = name;
 		this.hours = hours;
@@ -37,10 +43,23 @@ public class Employee {
 
 	public void setValuePerHour(Double valuePerHour) {
 		this.valuePerHour = valuePerHour;
-	}
+	}	
 	
+	public Double getSalary() {
+		return salary;
+	}
+
+	public void setSalary(Double salary) {
+		this.salary = salary;
+	}
+
 	public double payment() {
 		return hours * valuePerHour;
+	}
+
+	@Override
+	public int compareTo(Employee other) {
+		return -salary.compareTo(other.getSalary());
 	}
 	
 	
