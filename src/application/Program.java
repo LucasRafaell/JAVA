2f6d0c4f8d765f7b0de754b39dad9026b185e522
Lucas Rafael <lucasrafaelinfo@gmail.com>
2023-04-11 16:29:03 -0300

    Set com teste de igualdade por conteúdo

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,28 +1,21 @@
 package application;
 
-import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Set;
-import java.util.TreeSet;
+
+import entities.Product;
 
 public class Program {
 	public static void main(String[] args) {
 		
-		Set<Integer> a = new TreeSet<>(Arrays.asList(0,2,4,5,6,8,10));
-		Set<Integer> b = new TreeSet<>(Arrays.asList(5,6,7,8,9,10));
+		Set<Product> set = new HashSet<>();
 		
-		//Unior
-		Set<Integer> c = new TreeSet<>(a);
-		c.addAll(b);
-		System.out.println(c);
+		set.add(new Product("TV", 900.0));
+		set.add(new Product("Notebook", 1200.0));
+		set.add(new Product("Tablet", 400.0));
 		
-		//Intersection
-		Set<Integer> d = new TreeSet<>(a);
-		d.retainAll(b);
-		System.out.println(d);
+		Product prod = new Product("Notebook", 1200.0);
 		
-		//Diference
-		Set<Integer> e = new TreeSet<>(a);
-		e.removeAll(b);
-		System.out.println(e);
+		System.out.println(set.contains(prod));
 	}
 }
