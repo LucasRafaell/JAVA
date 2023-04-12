33bdb0dce31479b1a504fc7b99f1ff23d061b9d6
Lucas Rafael <lucasrafaelinfo@gmail.com>
2023-04-12 14:04:18 -0300

    Estrutura Map

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,47 +1,28 @@
 package application;
 
 import java.text.ParseException;
-import java.util.HashSet;
-import java.util.Scanner;
-import java.util.Set;
+import java.util.HashMap;
+import java.util.Map;
+
+import entities.Product;
 
 public class Program {
 
 	public static void main(String[] args) throws ParseException {
 
-		Scanner sc = new Scanner(System.in);
-
-		Set<Integer> a = new HashSet<>();
-		Set<Integer> b = new HashSet<>();
-		Set<Integer> c = new HashSet<>();
-
-		System.out.print("How many students for course A? ");
-		int n = sc.nextInt();
-		for (int i = 0; i < n; i++) {
-			int number = sc.nextInt();
-			a.add(number);
-		}
-
-		System.out.print("How many students for course B? ");
-		n = sc.nextInt();
-		for (int i = 0; i < n; i++) {
-			int number = sc.nextInt();
-			b.add(number);
-		}
-
-		System.out.print("How many students for course C? ");
-		n = sc.nextInt();
-		for (int i = 0; i < n; i++) {
-			int number = sc.nextInt();
-			c.add(number);
-		}
-
-		Set<Integer> total = new HashSet<>(a);
-		total.addAll(b);
-		total.addAll(c);
-
-		System.out.println("Total students: " + total.size());
-
-		sc.close();
+		Map<Product, Double> stock = new HashMap<>();
+		
+		Product p1 = new Product("TV", 900.0);
+		Product p2 = new Product("Notebook", 1200.0);
+		Product p3 = new Product("Tablet", 400.0);
+		
+		stock.put(p1, 10000.0);
+		stock.put(p2, 20000.0);
+		stock.put(p3, 15000.0);
+		
+		Product ps = new Product("TV", 900.0);
+		
+		System.out.println("Contains 'ps' key: " + stock.containsKey(ps));
+		
 	}
 }
